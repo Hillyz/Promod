e4228e4a6578bd0ffb191c451bafc3cdec0395c1
Hillyz <amund2003@gmail.com>
2020-11-13 13:35:38 +0100

    La in eksempel

---
+++ prøve.py
@@ -0,0 +1,20 @@
+#Oppgave 3
+
+#Konstaner
+c = 3 * 1E8
+lambda0 = [278, 490, 687, 856, 954, 399]
+
+    v_lst = [] #Unødvendig liste nå, kan være fin til senere hvis vi vil sammenlikne farten på stjernene
+    for i in range(len(lst1)):
+        v = round((lst1[i]-lst2[i])/ lst2[i] * c, 2) #regner ut farten og runder av til to desimaler
+        if v < 0:
+            print(f"Stjerne nummer {i+1} er blåforskjøvet med farten {v} m/s")
+        else:
+            print(f"Stjerne nummer {i+1} er rødforkjøvet med farten {v} m/s")
+
+        v_lst.append(v) #igjen unødvening, men kanskje interessant
+
+    return v_lst
+
+#programmet blir utført
+stjernefart(lambda0, lambda_obs)
